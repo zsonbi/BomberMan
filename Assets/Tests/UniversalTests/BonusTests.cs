25efec855c2a41be51b9d2e4306c9d045bb7fa28
Dér Zsombor Zsolt <y07jyd@inf.elte.hu>
2024-04-11 15:13:18 +0200

    Added some more bonus tests

--- Assets/Tests/UniversalTests/BonusTests.cs
+++ Assets/Tests/UniversalTests/BonusTests.cs
@@ -10,6 +10,7 @@ using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
+using static UnityEditor.Progress;
 
 namespace Tests
 {
@@ -76,6 +77,81 @@ namespace Tests
                     Assert.AreEqual(i, gameBoard.Players.First().Bonuses[BonusType.BombRange].Tier);
                 }
             }
+        }
+
+        [UnityTest]
+        public IEnumerator TestMultipleBonusesPickUp()
+        {
+            BonusType[] bonusTypesToTest = new BonusType[] { BonusType.BombRange, BonusType.BonusBomb };
+
+            MainMenuConfig.Player3 = false;
+            gameBoard.ForceSpecificMobTypeOnLoad(MonsterType.Basic);
+
+            gameBoard.StartNextGame();
+            gameBoard.CreateBoard("Maps/TestMaps/testMapEveryOneStuck2");
+            yield return null;
+
+            foreach (var item in bonusTypesToTest)
+            {
+                gameBoard.SpawnBonus(item, gameBoard.Players.First().CurrentBoardPos);
+            }
+
+            yield return new WaitForFixedUpdate();
+
+            foreach (var item in bonusTypesToTest)
+            {
+                Assert.IsTrue(gameBoard.Players.First().Bonuses.ContainsKey(item));
+                Assert.AreEqual(1, gameBoard.Players.First().Bonuses[BonusType.BombRange].Tier);
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator TestNoBombBonus()
+        {
+            MainMenuConfig.Player3 = false;
+            gameBoard.ForceSpecificMobTypeOnLoad(MonsterType.Basic);
+
+            gameBoard.StartNextGame();
+            gameBoard.CreateBoard("Maps/TestMaps/testMapEveryOneStuck2");
+            yield return null;
+            gameBoard.SpawnBonus(BonusType.NoBomb, gameBoard.Players.First().CurrentBoardPos);
+            float noBombLength = gameBoard.Entites[0].GetComponent<Bonus>().Duration;
+            yield return new WaitForFixedUpdate();
+
+            foreach (var control in gameBoard.Players[0].Controls)
+            {
+                if (control.Value.Method.Name == "PlaceBomb")
+                {
+                    control.Value.Method.Invoke(gameBoard.Players[0], null);
+                    Assert.AreEqual(gameBoard.Players[0].Bombs.Count, gameBoard.Players[0].Bombs.Count(x => !x.Placed));
+                }
+            }
+            yield return new WaitForSeconds(noBombLength + 0.1f);
+            foreach (var control in gameBoard.Players[0].Controls)
+            {
+                if (control.Value.Method.Name == "PlaceBomb")
+                {
+                    control.Value.Method.Invoke(gameBoard.Players[0], null);
+                    Assert.AreNotEqual(gameBoard.Players[0].Bombs.Count, gameBoard.Players[0].Bombs.Count(x => !x.Placed));
+                }
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator TestInstantBombBonus()
+        {
+            MainMenuConfig.Player3 = false;
+            gameBoard.ForceSpecificMobTypeOnLoad(MonsterType.Basic);
+
+            gameBoard.StartNextGame();
+            gameBoard.CreateBoard("Maps/TestMaps/testMapEveryOneStuck2");
+            yield return null;
+            gameBoard.SpawnBonus(BonusType.InstantBomb, gameBoard.Players.First().CurrentBoardPos);
+            yield return new WaitForFixedUpdate();
+            yield return new WaitForFixedUpdate();
+            yield return new WaitForFixedUpdate();
+
+            Assert.AreNotEqual(gameBoard.Players[0].Bombs.Count, gameBoard.Players[0].Bombs.Count(x => !x.Placed));
         }
     }
 }
