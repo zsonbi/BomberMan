2e9877542a00a17f6cc5825fbc088396d8f77165
Uzonyi <izmcir@inf.elte.hu>
2024-04-17 21:17:07 +0200

    Syncronized both mute toggles

--- Assets/Scripts/MainMenu/SoundManage.cs
+++ Assets/Scripts/MainMenu/SoundManage.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManage : MonoBehaviour
 {
+    /// <summary>
+    /// Set the toggle is on parameter
+    /// </summary>
+    public GameObject Toggle;
+
+    private void Start()
+    {
+        Toggle.GetComponent<Toggle>().isOn = MainMenuConfig.SoundMuted;
+    }
     /// <summary>
     /// Mute and unmute the background sound
     /// </summary>
@@ -12,11 +22,14 @@ public class SoundManage : MonoBehaviour
     {
         if (muted)
         {
+            MainMenuConfig.SoundMuted = true;
             AudioListener.volume = 0;
         }
         else
         {
+            MainMenuConfig.SoundMuted = false;
             AudioListener.volume = 0.2f;
         }
+        Toggle.GetComponent<Toggle>().isOn = MainMenuConfig.SoundMuted;
     }
 }
