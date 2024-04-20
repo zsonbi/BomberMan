2d5fa67e9ef56075e215604cd1533eb603d43d9e
Csóka Máté <qt9bq9@inf.elte.hu>
2024-04-20 21:15:02 +0200

    Implemented ObstacleSave

--- Assets/Scripts/Persistance/ObstacleSave.cs
+++ Assets/Scripts/Persistance/ObstacleSave.cs
@@ -2,11 +2,29 @@ using Bomberman;
 using DataTypes;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 namespace Persistance
 {
     public abstract class ObstacleSave : MapEntitySave
     {
+        public bool Placed { get; private set; }
+
+        public bool Destructible { get; private set; }
+
+        public BonusType ContainingBonusType { get; private set; }
+
+        public bool NotPassable { get; private set; }
+
+        public void SaveObstacle(Obstacle obstacleToSave)
+        {
+            this.Placed = obstacleToSave.Placed;
+            this.Destructible = obstacleToSave.Destructible;
+            this.ContainingBonusType = obstacleToSave.ContainingBonus.Type;
+            this.NotPassable = obstacleToSave.NotPassable;
+
+            base.Save(obstacleToSave);
+        }
     }
 }
