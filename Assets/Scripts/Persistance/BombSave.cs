43fef5bc63a85b1848ac1f234bf5641aba1cfa1f
Csóka Máté <qt9bq9@inf.elte.hu>
2024-04-20 21:08:39 +0200

    BombSave implemented

--- Assets/Scripts/Persistance/BombSave.cs
+++ Assets/Scripts/Persistance/BombSave.cs
@@ -6,13 +6,42 @@ using UnityEngine;
 
 namespace Persistance
 {
-    public abstract class BombSave : MapEntitySave
+    public class BombSave : MapEntitySave
     {
- 
+        /// <summary>
+        /// How far does the bomb is blasting
+        /// </summary>
+        public int BlastRadius { get; private set; }
 
-        public BombSave(Position currentBoardPos, MapEntityType mapEntityType) : base(currentBoardPos, mapEntityType)
+        /// <summary>
+        /// Is the bomb is currently placed and active
+        /// </summary>
+        public bool Placed { get; private set; }
+
+        /// <summary>
+        /// The bomb's internal timer
+        /// </summary>
+        public float BombTimer { get; private set; }
+
+        /// <summary>
+        /// How long does the bomb take to blow up
+        /// </summary>
+        public float TimeTillBlow { get; private set; } = Config.BOMBBLOWTIME;
+
+        /// <summary>
+        /// Is the bomb detonable
+        /// </summary>
+        public bool Detonable { get; set; } = false;
+
+        public void SaveBomb(Bomb bombToSave)
         {
-            this.CurrentBoardPos = currentBoardPos;
+            this.BlastRadius = bombToSave.BlastRadius;
+            this.Placed = bombToSave.Placed;
+            this.BombTimer = bombToSave.BombTimer;
+            this.TimeTillBlow = bombToSave.TimeTillBlow;
+            this.Detonable = bombToSave.Detonable;
+
+            base.Save(bombToSave);
         }
     }
 }
--- Assets/Scripts/Persistance/MonsterSave.cs
+++ Assets/Scripts/Persistance/MonsterSave.cs
@@ -8,19 +8,5 @@ namespace Persistance
 {
     public abstract class MonsterSave : MovingEntitySave
     {
-        /// <summary>
-        /// The entity's current position on the Board's grid
-        /// </summary>
-        public Position CurrentBoardPos { get; protected set; }
-
-        /// <summary>
-        /// The type of the entity
-        /// </summary>
-        public MapEntityType EntityType { get; protected set; }
-
-        public MonsterSave(Position currentBoardPos, MapEntityType mapEntityType) : base(currentBoardPos, mapEntityType)
-        {
-            this.CurrentBoardPos = currentBoardPos;
-        }
     }
 }
